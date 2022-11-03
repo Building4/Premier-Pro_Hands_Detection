3f19d975852adf3876ac854730cd06047417ca79
liam <nonameproductions12321@gmail.com>
2022-11-03 19:21:07 +0200

    hi

--- actions.py
+++ actions.py
@@ -1,5 +1,14 @@
+import pyautogui as pg
+def rightLeftMovement(a=0):
+  b=0
+  while True:
+   if(a>0 and b<a):
+      pg.hotkey("right")
+      b=b+1
+   if(a<0 and b>a):
+     pg.hotkey("left")
+     b=b-1
 
-def Right_Frame():
-  print("one frame to the right")
-  def Left_Frame():
-  print("one frame to the Left")
+  
+    
+  
